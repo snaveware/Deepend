b803cba09787090599db6930eae7fc530c51b2be
Evans Munene <work.evans020@gmail.com>
2020-06-11 09:53:14 +0300

    change database configurations

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'deepend',
+	'hostname' => 'remotemysql.com',
+	'username' => 'sF8vybLsih',
+	'password' => 'Bb6cyK9Zmy',
+	'database' => 'sF8vybLsih',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
