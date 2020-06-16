70ee430f19499b806ed80dff60bebb35ff6e4df7
Evans Munene <work.evans020@gmail.com>
2020-06-16 11:28:30 +0300

    correcting a database error

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'https://deepend.herokuapp.com/';
-$config['base_url'] = 'http://localhost/deepend/';
+$config['base_url'] = 'https://deepend.herokuapp.com/';
+//$config['base_url'] = 'http://localhost/deepend/';
 
 /*
 |--------------------------------------------------------------------------
