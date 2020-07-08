106f49a1360719190a1df2e6d4dcfb989b0c4360
Evans Munene <work.evans020@gmail.com>
2020-07-08 11:22:41 +0300

    email testing

--- application/config/email.php
+++ application/config/email.php
@@ -2,7 +2,7 @@
   $config['protocal']="smtp";
   $config['smtp_host'] = "smtp.gmail.com";
   $config['smtp_user'] ="work.evans020@gmail.com";
-  $config['smtp_pass'] ="";
+  $config['smtp_pass'] ="Je_taime20449682";
   $config['smtp_port'] = 25;
   $config['smtp_timeout'] = 10;
   $config['smtp_crypto'] = "tls";
